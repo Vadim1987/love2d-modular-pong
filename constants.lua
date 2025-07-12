50d53682c13cd93e93b85ae53374a91d1eba46ce
Vadim1987 <vadim.stetskiy@gmail.com>
2025-07-13 00:55:49 +0200

    add horizontal movement to paddles and WASD/Arrow controls
    - Both paddles can now move horizontally up to the field's center.
    - Player controls: WASD (left) / Arrow keys (right).
    - New constants for horizontal limits and speed.

--- constants.lua
+++ constants.lua
@@ -1,5 +1,3 @@
--- All constants in one place
-
 -- Window dimensions
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
@@ -7,10 +5,15 @@ WINDOW_HEIGHT = 600
 -- Paddle settings
 PADDLE_WIDTH = 10
 PADDLE_HEIGHT = 60
-PADDLE_SPEED = 300
+PADDLE_SPEED = 300         -- Vertical speed
+PADDLE_HSPEED = 200        -- Horizontal speed
 
--- Ball settings
-BALL_SIZE = 10
+-- Horizontal paddle movement limits (not beyond half field)
+BAT_MAX_X = (WINDOW_WIDTH / 2) - PADDLE_WIDTH - 10      -- For player (left)
+BAT_MIN_X_OPP = (WINDOW_WIDTH / 2) + 10                 -- For opponent (right)
+
+-- Ball settings (now circle)
+BALL_SIZE = 10              -- This is the puck RADIUS!
 BALL_SPEED_X = 240
 BALL_SPEED_Y = 120
 
@@ -18,10 +21,10 @@ BALL_SPEED_Y = 120
 WIN_SCORE = 10
 
 -- Positioning
-PADDLE_OFFSET_X = 30
-SCORE_OFFSET_Y = 40
+PADDLE_OFFSET_X = 30        -- Initial paddle offset from side
+SCORE_OFFSET_Y = 40         -- Score display height
 
--- Colors
+-- Colors (RGB in [0,1])
 COLOR_BG = {0, 0, 0}
 COLOR_FG = {1, 1, 1}
 
